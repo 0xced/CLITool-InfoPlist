a8b7c41a919c94acf8755612c33af212adde721e
Cédric Luthi <cedric.luthi@gmail.com>
2010-10-21 16:22:14 +0200

    Dump __TEXT,__info_plist section even when it is binary encoded

--- dump_info_plist/dump_info_plist.m
+++ dump_info_plist/dump_info_plist.m
@@ -1,6 +1,7 @@
 #import <Foundation/NSAutoreleasePool.h>
 #import <Foundation/NSBundle.h>
 #import <Foundation/NSData.h>
+#import <Foundation/NSPropertyList.h>
 #import <Foundation/NSString.h>
 #import <mach-o/getsect.h>
 #import <stdio.h>
@@ -19,9 +20,15 @@ int main (int argc, const char * argv[])
 	NSData *plist = [NSData dataWithBytesNoCopy:(void*)__info_plist->addr length:__info_plist->size freeWhenDone:NO];
 	NSData *xmlSignature = [NSData dataWithBytesNoCopy:"<?xml" length:5 freeWhenDone:NO];
 	if ([[plist subdataWithRange:NSMakeRange(0, [xmlSignature length])] isEqualToData:xmlSignature])
-		printf("raw __info_plist section:\n-------------------------\n%s\n", [[[[NSString alloc] initWithData:plist encoding:NSUTF8StringEncoding] autorelease] UTF8String]);
+	{
+		NSString *xml = [[[NSString alloc] initWithData:plist encoding:NSUTF8StringEncoding] autorelease];
+		printf("raw __info_plist section:\n-------------------------\n%s\n", [xml UTF8String]);
+	}
 	else
-		printf("__info_plist section is binary encoded\n");
+	{
+		id infoPlist = [NSPropertyListSerialization propertyListFromData:plist mutabilityOption:NSPropertyListImmutable format:NULL errorDescription:NULL];
+		printf("binary __info_plist section:\n----------------------------\n%s\n", [[infoPlist description] UTF8String]);
+	}
 	
 	NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
 	printf("\nmainBundle infoDictionary:\n--------------------------\n%s\n", [[infoDictionary description] UTF8String]);
